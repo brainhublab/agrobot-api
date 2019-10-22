edb29701114e17d81d0fecdfa3d6aa03c31b277f
Dimitar Peev <dpeev95@gmail.com>
2019-10-22 17:30:31 +0300

    Add env file to rules_storage_api container.

--- event_rules_storage_api/instance/config.py
+++ event_rules_storage_api/instance/config.py
@@ -7,16 +7,16 @@ SECRET_KEY = 'p9Bv<3Eid9%$i01'
 
 HOST = "http://localhost:8081/"
 
-# POSTGRES_DB = os.environ.get("POSTGRES_DB")
-# POSTGRES_USER = os.environ.get("POSTGRES_USER")
-# POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-# POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
 
-SQLALCHEMY_DATABASE_URI = "postgresql://testadmin:testtest@db/testdb"
+# SQLALCHEMY_DATABASE_URI = "postgresql://testadmin:testtest@db/testdb"
 
 TOKEN = "fe742bcb7bfa0c3ff680be5f84118321c2d2088b"
-# SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(POSTGRES_USER,
-#                                                             POSTGRES_PASSWORD,
-#                                                             POSTGRES_HOST,
-#                                                             POSTGRES_DB)
+SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(POSTGRES_USER,
+                                                            POSTGRES_PASSWORD,
+                                                            POSTGRES_HOST,
+                                                            POSTGRES_DB)
 
